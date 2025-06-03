88e86b69c068f7dc1bc7e55c2e87a7868c740c49
lukasGeisseler <lukas@geisseler.info>
2025-06-04 01:55:37 +0200

    updated perceptron

--- src/models/perceptron.py
+++ src/models/perceptron.py
@@ -1,183 +1,145 @@
-# Train the perceptron
-
-from typing import Tuple
+import os
+import sys
+import logging
+from enum import Enum
 import numpy as np
 
-from src.preprocessing.data_transforms import shuffle_data, normalize
+# Add the project root directory to sys.path to allow for absolute imports
+# Assuming this script is in src/models/, the project root is two levels up.
+if __name__ == "__main__":
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    sys.path.insert(0, project_root)
+
+from src.utils.load_data import mnist
+from src.preprocessing.data_transforms import normalize, shuffle_data, one_hot_encoding
+from src.components.initializers import initialize_randn_weights
 from src.components.activations import step_function
 from src.components.blocks import compute_weighted_sum
+from src.utils.plot import plot_metrics
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO
+)
+
+class ExtendedEnum(Enum):
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+class Activations(ExtendedEnum):
+    STEP_FUNCTION = step_function
+    # SIGMOID = sigmoid
+    # RELU = relu
 
 
-def predict(
-    transformed_image: np.ndarray,
-    weights: np.ndarray,
-    bias_size: int = 1,
-    backward=False,
-    idx2label: dict = None
-) -> int:
-    """
-    Perform a forward pass through the perceptron.
-
-    Args:
-        transformed_image (np.ndarray): The input image after transformation.
-        weights (np.ndarray): The weights of the perceptron.
-        bias_size (int): The size of the bias term.
-        backward (bool): If True, return idx otherwise return the label.
-
-    Returns:
-        int: The prediction made by the perceptron.
-    """
-    # apply random conections from the input to the assosciated weights
-
-    # binary classifier case
-    if len(weights.shape) == 1:
-        # Calculate the weighted sum of inputs and weights
-        weighted_sum = compute_weighted_sum(transformed_image, weights, bias_size)
-
-        # Apply the activation function
-        preds = step_function(weighted_sum, 0)
-        return preds if backward else idx2label.get(preds.item())
-
-    # Multiclass case
-    else:
-        preds = np.zeros(weights.shape[0])
-
-        # Calculate the weighted sum of inputs and weights for each class
-        for i in range(weights.shape[0]):
-            weighted_sum = compute_weighted_sum(
-                transformed_image, weights[i], bias_size
-            )
-
-            # Apply the activation function add the result to the predictions
-            preds[i] = step_function(weighted_sum, 0)
-
-        # Return the index of the class with the highest prediction
-        return np.argmax(preds)
-
-
-def backward_pass(
-    transformed_image: np.ndarray,
-    weights: np.ndarray,
-    label: int,
-    learning_rate: float = 0.1,
-    bias_size: int = 1,
-    label2idx: dict = None,
-    idx2label: dict = None,
-) -> Tuple[np.ndarray, float]:
-    """
-    Perform a backward pass through the neural network.
-
-    Args:
-        transformed_image (np.ndarray): The input image after transformation.
-        weights (np.ndarray): The weights of the neural network.
-        label (int): The true label of the image.
-        learning_rate (float, optional): The learning rate for weight updates.
-        bias_size (int, optional): amount of additional weights for the bias term. Defaults to 1.
-
-    Returns:
-        Tuple[np.ndarray, float]: Updated weights and the error.
-    """
-
-    # Forward pass
-    prediction = predict(transformed_image, weights, bias_size, backward=True, idx2label=idx2label)
-
-    # Binary classifier case
-    if len(weights.shape) == 1:
-        # Calculate the error
-        error = label2idx.get(label) - prediction
-
-        # Update weights and bias
-        weights[:-bias_size] += learning_rate * error * transformed_image
-        weights[-bias_size] += learning_rate * error
-
-        loss = abs(error)
-
-    # Multiclass case
-    else:
-        loss = 0
-        if prediction != label:
-            # Calculate the error
-            loss = 1
-
-            # update the weights
-            # first decrease the weights for the misclassified class as the error is 1, we don't need to multiply with the error
-            weights[prediction][:-bias_size] -= learning_rate * transformed_image
-            weights[prediction][-bias_size] -= learning_rate
-            # then increase the weights for the correct class
-            weights[label][:-bias_size] += learning_rate * transformed_image
-            weights[label][-bias_size] += learning_rate
-
-    return weights, loss
-
-
-def train_perceptron(
-    X_train: np.ndarray,
-    y_train: np.ndarray,
-    epochs: int = 1,
-    learning_rate: float = 0.1,
-    bias_size: int = 1,
-) -> np.ndarray:
-    """trains the perceptron using the training data.
-
-    Args:
-        X_train (np.ndarray): training data
-        y_train (np.ndarray): training labels
-        epochs (int, optional): number of epochs. Defaults to 100.
-        learning_rate (float, optional): learning rate. Defaults to 0.1.
-
-    Returns:
-        np.ndarray: weights
-    """
-
-    idx2label = dict(enumerate(np.unique(y_train)))
-    label2idx = {v: k for k, v in idx2label.items()}
-
-    # initialize weights
-    w_len = X_train[0].flatten().shape[0]
-    n_classes = np.unique(y_train).shape[0]
-    X_train = normalize(X_train, 0, 1).round(decimals=0)
-    # if we have a binary classifier, we need only one array of weights else we need n_classes of weightarrays
-    weights = (
-        np.zeros(w_len + bias_size)
-        if n_classes == 2
-        else np.zeros((n_classes, w_len + bias_size))
+def zero_initializer(
+    n_inputs: int, n_classes: int, bias_size: int = 1, mean: float = 0, std: float = 1
+):
+    return np.zeros_like(
+        initialize_randn_weights(n_inputs, n_classes, bias_size, mean=mean, std=std)
     )
-    # if you want to use random connections for the weight input, you can uncomment the following line
-    # random_weight = step_function(np.diag(np.random.rand(w_len)), 0) # omitted random connections
 
-    # initialize metrics
-    train_metrics = {"loss": np.zeros(epochs), "accuracy": np.zeros(epochs)}
 
-    # iterate over the epochs
-    for epoch in range(epochs):
-        # iterate over the training data
+class Initializers(ExtendedEnum):
+    RANDOM_NORMAL = initialize_randn_weights
+    ZERO = zero_initializer
 
-        # shuffle the data
-        X_train, y_train = shuffle_data(X_train, y_train)
+print(getattr(Initializers, "RANDOM_NORMAL"))
 
-        epoch_loss = 0
-        for image, label in zip(X_train, y_train):
-            # transform the image to a binary vector
-            transformed_image = image.flatten()
-            # transformed_image = np.matmul(random_weight, transformed_image) # omitted random connections
-            weights, loss = backward_pass(
-                transformed_image,
-                label=label,
-                weights=weights,
-                learning_rate=learning_rate,
-                bias_size=bias_size,
-                label2idx=label2idx,
-                idx2label=idx2label,
+class Perceptron:
+    def __init__(
+        self, activation: str = "step_function", initializer: str = "random_normal"
+    ):
+        self.bias_size = 1
+        self.activation = Activations.STEP_FUNCTION  # get(activation.upper())
+        self.initializer = getattr(Initializers, initializer.upper())
+          # get(initializer.upper())
+        self.learning_rate = None
+        self.epochs = None
+        self.weights = None
+        self.x = None
+        self.y = None
+        self.num_labels = None
+        self.num_images = None
+        self.num_features = None
+        self.metrics = None
+
+    def predict(self, X: np.ndarray, **kwargs) -> np.ndarray:
+        weighted_sum = compute_weighted_sum(X, self.weights.T, self.bias_size)
+        return np.argmax(
+            self.activation(weighted_sum, threshold=kwargs.get("threshold", 0.0))
+        )
+
+    def backward_pass(self, x, y, **kwargs) -> int:
+        pred = self.predict(x, kwargs=kwargs)
+        if pred != np.argmax(y):
+            error_signal = y - one_hot_encoding(pred, self.num_labels)
+            # print(error_signal)
+            ## same as decrease the weights for the misclassified class and increase for the correct
+            ## here we have a 1 for the correct class and -1 for the incorrect class
+            update = (self.learning_rate * error_signal).reshape(-1, 1) * np.stack(
+                [np.hstack((x, np.ones(self.bias_size)))] * self.num_labels
             )
-            epoch_loss += loss
-        # as we use only binary values of 0 and 1, the loss is the number of misclassified images
-        # we can calculate the accuracy as well
+            ## therefore we should add the update to the weights
+            self.weights += update
+            return 1
+        return 0
 
-        train_metrics["loss"][epoch] = epoch_loss
-        train_metrics["accuracy"][epoch] = (len(y_train) - epoch_loss) / len(y_train)
-        
-        if epoch % (epochs // 10 if epochs >= 10 else 1) == 0 or epoch == epochs - 1:
-            print(
-                f"Epoch {epoch}: Loss: {epoch_loss:.4f}, Accuracy: {train_metrics['accuracy'][epoch]:.4f}"
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray, **kwargs):
+        self.num_labels = np.unique(y_train).shape[0]
+        self.x = normalize(X_train.reshape(X_train.shape[0], -1), 0, 1).round(
+            decimals=0
+        )
+        self.num_images, self.num_features = self.x.shape
+        self.y = one_hot_encoding(y_train, num_labels=len(np.unique(y_train)))
+        self.weights = self.initializer(
+            self.num_features, self.num_labels + 1, self.bias_size, 0.0, 0.01
+        )[:-1]  # hack to omit binary weight altohg there are only 2 classes present.
+        # self.weights = np.zeros_like(self.weights)
+        self.epochs = kwargs.get("epochs", 10)
+        self.learning_rate = kwargs.get("learning_rate", 0.1)
+        self.bias_size = kwargs.get("bias_size", 1)
+        self.metrics = {
+            "loss": np.zeros(self.epochs),
+            "accuracy": np.zeros(self.epochs),
+        }
+
+        logging.info("Starting training for %d epochs", self.epochs)
+        for epoch in range(self.epochs):
+            x, y = shuffle_data(self.x, self.y.T)
+
+            epoch_loss = sum(
+                self.backward_pass(image, label, kwargs=kwargs)
+                for image, label in zip(x, y)
             )
-    return weights, train_metrics
+            self.metrics["loss"][epoch] = epoch_loss
+            self.metrics["accuracy"][epoch] = (
+                self.num_images - epoch_loss
+            ) / self.num_images
+
+            if (
+                epoch % (self.epochs // 10 if self.epochs >= 10 else 1) == 0
+                or epoch == self.epochs - 1
+            ):
+                logging.info(
+                    "Epoch %d: Loss: %.4f, Accuracy: %.4f",
+                    epoch,
+                    epoch_loss,
+                    self.metrics["accuracy"][epoch],
+                )
+        if kwargs.get("plot_metrics"):
+            plot_metrics(self.metrics)
+
+    def eval(self, X_test: np.ndarray, y_test: np.ndarray, **kwargs):
+        x = normalize(X_test.reshape(X_test.shape[0], -1), 0, 1).round(decimals=0)
+        preds = [self.predict(image, kwargs=kwargs) for image in x]
+        self.metrics["test_accuracy"] = np.mean(preds == y_test)
+        return self.metrics["test_accuracy"]
+
+
+if __name__ == "__main__":
+    X_train, y_train, X_test, y_test = mnist()
+    perceptron = Perceptron(activation="step_function", initializer="zero")
+    perceptron.fit(X_train, y_train, epochs=10, learning_rate=0.001, threshold=0.5)
+    logging.info("Test accuracy: %.4f", perceptron.eval(X_test, y_test, threshold=0.5))
--- src/utils/plot.py
+++ src/utils/plot.py
@@ -5,7 +5,6 @@ from typing import Dict, Iterable
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 # plot images
 def plot_examples(
     images: Iterable,
